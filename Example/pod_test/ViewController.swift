0da1fd66981c786dbc2eba0d440b33e4a5b3f253
g <g@gdeMac-mini.local>
2019-03-14 11:19:25 +0800

    Initial commit

---
+++ Example/pod_test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  pod_test
+//
+//  Created by [Ba752538236 on 03/14/2019.
+//  Copyright (c) 2019 [Ba752538236. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
